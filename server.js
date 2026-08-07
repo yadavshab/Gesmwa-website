0dff8fc8823bcf6a9895b5242b652eca1379b873
Mayank Yadav <136424434+yadavshab@users.noreply.github.com>
2026-08-07 18:34:37 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ require('dotenv').config();
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // MongoDB Connection
 const MONGO_URI = process.env.MONGO_URI || "";
